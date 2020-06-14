6838de523da7f386a0e2e5a15cb6068d953e2290
jok3r69 <kristof.kondrat@gmail.com>
2020-06-14 22:26:58 +0300

    major changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,12 @@
-class Obstacle {
-    TOP_BOUNDERIES = 0;
-    BOTTOM_BOUNDERIES = 0;
-    LEFT_BOUNDERIES = 0;
-    RIGHT_BOUNDERIES = 0;
+const board = document.querySelector('.board');
 
-    constructor(obstacle) {
-        const rect = obstacle.getBoundingClientRect();
-        this.TOP_BOUNDERIES = rect.top;
-        this.BOTTOM_BOUNDERIES = rect.bottom;
-        this.LEFT_BOUNDERIES = rect.left;
-        this.RIGHT_BOUNDERIES = rect.right;
-    }
-}
-//put the obtacles in an arrey
-//all obscatles need to have 4 bounderies
-//
-const keyMap= {
-    TOP: 38,
-    RIGHT: 39,
-    BOTTOM: 40,
-    LEFT: 37,
-}
+// constant(like PI math constants not const declared) variables
+const keyMap = {
+  TOP: 38,
+  RIGHT: 39,
+  BOTTOM: 40,
+  LEFT: 37,
+};
 
 const PLAYER_MOVE_STEP = 20;
 const MAP_WIDTH = 1000;
@@ -28,140 +14,218 @@ const MAP_HEIGHT = 500;
 const PLAYER_WIDTH = 20;
 const PLAYER_HEIGHT = 20;
 
-class Player {
-    element = document.querySelector('.player');
-    x= 0;
-    y= 0;
-    
-    //when new Player is called, `this` is created
-    constructor() {
-        //console.log(this.element)
-        this._initMovement();
+class Obstacle {
+  element;
+  x;
+  y;
+  width;
+  height;
+
+  constructor(x, y, width, height, position = 'top') {
+    this._createElement();
+
+    this.x = x;
+    this.y = y;
+    if (position === 'bottom') {
+      this.y = MAP_HEIGHT - height - y;
     }
-    
-    _initMovement() {
-        // console.log(this);
-        document.addEventListener('keydown', this._hangleMovement.bind(this));
-    }
-    
-    _hangleMovement(event) {
-        switch(event.keyCode) {
-            case keyMap.TOP: {
-                this.moveTop();
-                break;
-            }
-            case keyMap.RIGHT: {
-                this.moveRight();                
-                break;
-            }
-            case keyMap.BOTTOM: {
-                this.moveBottom();
-                break;
-            }
-            case keyMap.LEFT: {
-                this.moveLeft();
-                break;
-            }
-        }
-    }
-    
-    moveTop() {
-        const newY = this.y - PLAYER_MOVE_STEP;
-        
-        if(this._isMoveInBounderies(this.x, newY)) {
-            this.y = newY;
-            this._updatePosition();            
-        }
-    }
-    moveRight() {
-        const newX = this.x + PLAYER_MOVE_STEP;
-        for(var i = 0 ; i < 5; i++) {
-            var obs = obstacles[i];
-            if (newX >= obs.LEFT_BOUNDERIES -15 && newX <= obs.RIGHT_BOUNDERIES -15 && this.y<obs.BOTTOM_BOUNDERIES){
-                console.log('touched');
-                console.log(`bot:${obs.BOTTOM_BOUNDERIES} left:${obs.LEFT_BOUNDERIES} right:${obs.RIGHT_BOUNDERIES}`);
-                console.log(`player_x:${newX} player_y:${this.y}`);
-            }
-        };
-        if(this._isMoveInBounderies(newX, this.y)) {
-            this.x = newX;
-            this._updatePosition();            
-        }
-    }
-    moveBottom() {
-        const newY = this.y + PLAYER_MOVE_STEP;
-        if(this._isMoveInBounderies(this.x, newY)) {
-            this.y = newY;
-            this._updatePosition();            
-        }
-    }
-    moveLeft() {
-        const newX = this.x - PLAYER_MOVE_STEP;
-        if(this._isMoveInBounderies(newX, this.y)) {
-            this.x = newX;
-            this._updatePosition();
-        }
-    }
-    
-    _updatePosition() {
-        this.element.style.top = `${this.y}px`;
-        this.element.style.left = `${this.x}px`;
-        
-    }
-    
-    _isMoveInBounderies(x, y) {
-        
-        if (y < 0) {
-            return false;
-        }
-        
-        if(x < 0){
-            return false;
-        }
-        if (x > MAP_WIDTH - PLAYER_WIDTH) {
-            return false;
-        }
-        
-        if (y > MAP_HEIGHT - PLAYER_HEIGHT) {
-            return false;
-        }
-        
-        return true;
-    }
+    this.width = width;
+    this.height = height;
+
+    this._setPosition();
+  }
+
+  _createElement() {
+    this.element = document.createElement('div');
+    this.element.classList.add('obstacle');
+    board.appendChild(this.element);
+  }
+
+  _setPosition() {
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+  }
+
+  remove() {
+    this.element.remove();
+  }
 }
+
+const obstacles = [
+  new Obstacle(100, 0, 20, 400),
+  new Obstacle(200, 0, 20, 400, 'bottom'),
+  new Obstacle(300, 0, 20, 400),
+  new Obstacle(400, 0, 20, 400, 'bottom'),
+  new Obstacle(500, 0, 20, 400),
+  new Obstacle(600, 0, 20, 400, 'bottom'),
+  new Obstacle(700, 0, 20, 400),
+  new Obstacle(800, 0, 20, 400, 'bottom'),
+  new Obstacle(900, 0, 20, 400),
+];
+
+class Player {
+  element = document.querySelector('.player');
+  x = 0;
+  y = 0;
+  width = PLAYER_WIDTH;
+  height = PLAYER_HEIGHT;
+
+  // when new Player is called, `this` is created
+  constructor() {
+    // console.log(this.element)
+    this._initMovement();
+  }
+
+  _initMovement() {
+    document.addEventListener('keydown', this._handleMovement.bind(this));
+  }
+
+  _handleMovement(event) {
+    switch (event.keyCode) {
+      case keyMap.TOP: {
+        this.moveTop();
+        break;
+      }
+      case keyMap.RIGHT: {
+        this.moveRight();
+        break;
+      }
+      case keyMap.BOTTOM: {
+        this.moveBottom();
+        break;
+      }
+      case keyMap.LEFT: {
+        this.moveLeft();
+        break;
+      }
+    }
+
+    if (this._isCollided()) {
+      alert('We collided');
+      this._resetPosition();
+    }
+  }
+
+  _resetPosition() {
+    this.x = 0;
+    this.y = 0;
+    this._updatePosition();
+  }
+
+  _isCollided() {
+    for (let i = 0; i < obstacles.length; i++) {
+      if (isCollision(this, obstacles[i])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  moveTop() {
+    const newY = this.y - PLAYER_MOVE_STEP;
+
+    if (this._isMoveInBoundaries(this.x, newY)) {
+      this.y = newY;
+      this._updatePosition();
+    }
+  }
+
+  moveRight() {
+    const newX = this.x + PLAYER_MOVE_STEP;
+
+    if (this._isMoveInBoundaries(newX, this.y)) {
+      this.x = newX;
+      this._updatePosition();
+    }
+  }
+
+  moveBottom() {
+    const newY = this.y + PLAYER_MOVE_STEP;
+
+    if (this._isMoveInBoundaries(this.x, newY)) {
+      this.y = newY;
+      this._updatePosition();
+    }
+  }
+
+  moveLeft() {
+    const newX = this.x - PLAYER_MOVE_STEP;
+
+    if (this._isMoveInBoundaries(newX, this.y)) {
+      this.x = newX;
+      this._updatePosition();
+    }
+  }
+
+  _updatePosition() {
+    this.element.style.top = `${this.y}px`;
+    this.element.style.left = `${this.x}px`;
+  }
+
+  _isMoveInBoundaries(x, y) {
+    if (y < 0) {
+      return false;
+    }
+
+    if (x < 0) {
+      return false;
+    }
+
+    if (x > MAP_WIDTH - PLAYER_WIDTH) {
+      return false;
+    }
+
+    if (y > MAP_HEIGHT - PLAYER_HEIGHT) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+class GameEngine {
+  lives = 3;
+
+  constructor() {
+    new Player(); // player only moves up and down
+
+    setTimeout(this.loop.bind(this), 100);
+  }
+
+  loop() {
+    // create obstacle until reaching limit
+    // move all obstacles to left 1 unit
+    // check collision
+  }
+
+}
+
+
+new GameEngine();
 
 const p1 = new Player();
 
-const obstacles = new Array();
-
-elementTop = document.querySelectorAll('.obstacleTop');
-console.log(elementTop);
-Array.from(elementTop).map(element => {
-    console.log(element);
-    const obj = new Obstacle(element);
-    obstacles[obstacles.length] = obj;
-});
-
-// for (i= 1; i < 7;  i++){
-//     console.log("obstacle" + i);
-//     obstacles[i] = new Obstacle("#obstacle" + i);
-// }
-
-
 // const pl = {
-    //     key: 'player',
-    //     callMe: function() {
-        //         console.log(this);
-        //         return this;
-        //     },
-        // };
-        
-        // pl.callMe(); //pl -> this
-        
-// const doc = {
-// key: 'document'
+//   key: 'player',
+//   test: function() {
+//     console.log('test is called')
+//   },
+//   callMe: function () {
+//     console.log(this);
+//     this.test();
+//   },
 // };
+
+// pl.callMe(); // pl -> this
+
+// const doc = {
+//   key: 'document',
+// };
+
 // // with bind we can hardcode the execution context(`this`)
 // doc.callMe = pl.callMe.bind(pl);
-
-// doc.callMe();
+// // console.log(doc);
+// doc.callMe(); //
